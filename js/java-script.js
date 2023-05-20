ba35acfc3aaee44c5556f37f912843752bbe927e
Anieb22 <anielloverdino@gmail.com>
2023-05-20 02:07:07 +0200

    fixato javascript (esercizio concluso, da aggiungere bonus)

--- js/java-script.js
+++ js/java-script.js
@@ -1,41 +1,16 @@
-const size = 12;
-let multi5 = 'buzz';
-let multi3 = 'fuzz';
-let multi3_5 = 'fuzz-buzz';
-let board = '';
-
 const output = document.getElementById('output')
 
-for(let row = 1; row <= size; row++){
-    board += "<div class='board-row'>";
+for (let number = 1; number <= 100; number++) {
 
-    if(row % 3 !==0){
-        multi3 = 'red'
+    if (number % 3 == 0 && number % 5 == 0) {
+
+      console.log('FizzBuzz');
+    } else if (number % 3 == 0) {
+      console.log('Fizz');
+    } else if (number % 5 == 0) {
+      console.log('Buzz');
+    } else {
+      console.log(number);
     }
-
-    else if(row % 5 !==0){
-        multi3 = 'yellow'
-    }
-
-    else(row % 3 !==0 && row % 5 !==0)
-        multi3_5 = 'orange'
-
-    for(let cell = 1; cell<= size; cell++){
-
-        if(cell % 3 !== 0){
-            board += '<div class"square ${multi3}"';
-        }
-
-        else if(cell % 5 !== 0){
-            board += '<div class"square ${multi5}"';
-        }
-
-        else(cell % 3 !==0 && cell % 5 !==0)
-            board += '<div class"square ${multi3_5}"';
-    }
-
-    board += '</div>';
-    
-}
-
-output.innetHTML += board;
+  
+  }
