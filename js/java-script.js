1990208610bcd5b36a5fbafc7adb427c3260fc4c
Anieb22 <anielloverdino@gmail.com>
2023-05-22 15:39:04 +0200

    definito BONUS 1

--- js/java-script.js
+++ js/java-script.js
@@ -1,16 +1,22 @@
-const output = document.getElementById('output')
+let output = document.getElementById('output');
+let string = '';
 
-for (let number = 1; number <= 100; number++) {
+for (let i = 1; i <= 100; i++) {
 
-    if (number % 3 == 0 && number % 5 == 0) {
-
+    if (i % 3 == 0 && i % 5 == 0) {
+      string += '<div>FizzBuzz</div>';
       console.log('FizzBuzz');
-    } else if (number % 3 == 0) {
+    } else if (i % 3 == 0) {
+      string += '<div>Fizz</div>';
       console.log('Fizz');
-    } else if (number % 5 == 0) {
+    } else if (i % 5 == 0) {
+      string += '<div>Buzz</div>';
       console.log('Buzz');
     } else {
-      console.log(number);
+      string += `<div>${i}</div>`;
+      console.log(i);
     }
   
   }
+
+  output.innerHTML += string;
