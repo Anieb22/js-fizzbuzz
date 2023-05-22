dc4677d89f522d87b1c3c3f5b1bb52abeb5002a0
Anieb22 <anielloverdino@gmail.com>
2023-05-22 16:08:40 +0200

    eseguito BONUS 2

--- js/java-script.js
+++ js/java-script.js
@@ -2,21 +2,30 @@ let output = document.getElementById('output');
 let string = '';
 
 for (let i = 1; i <= 100; i++) {
+  let elem = document.createElement('div')
+  elem.classList.add('square')
 
     if (i % 3 == 0 && i % 5 == 0) {
-      string += '<div>FizzBuzz</div>';
+      elem.classList.add('orange')
+      elem.innerText = 'FizzBuzz'
       console.log('FizzBuzz');
+
     } else if (i % 3 == 0) {
-      string += '<div>Fizz</div>';
+      elem.classList.add('red')
+      elem.innerText = 'Fizz'
       console.log('Fizz');
+
     } else if (i % 5 == 0) {
-      string += '<div>Buzz</div>';
+      elem.classList.add('yellow')
+      elem.innerText = 'Buzz'
       console.log('Buzz');
+
     } else {
-      string += `<div>${i}</div>`;
+      elem.classList.add('green')
+      elem.innerText = i
       console.log(i);
     }
-  
+
+    output.append(elem);
   }
 
-  output.innerHTML += string;
